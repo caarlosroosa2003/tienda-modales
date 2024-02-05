0b391413adb93f4b412199be9de7e3b785ce8367
caarlosroosa2003 <119525632+caarlosroosa2003@users.noreply.github.com>
2024-02-05 18:42:56 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'tienda';
+
+  sacarModal(name: string, text: string) {
+    Swal.fire({
+      icon: "question",
+      title: "¿Desea añadir al carrito este producto: " + name + "?",
+      text: text,
+      showDenyButton: true,
+      confirmButtonText: "Añadir",
+      denyButtonText: `No anadir`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Se ha añadido correctamente", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Se ha cancelado la operación", "", "error");
+      }
+    });
+  }
 }
